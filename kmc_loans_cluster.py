68d319e2f763c3406a6c974facb5e50fd03aa55d
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-11-12 17:56:03 -0500

    kmeans function

--- kmc_loans_cluster.py
+++ kmc_loans_cluster.py
@@ -2,7 +2,10 @@ import pandas
 import numpy
 # import random
 import matplotlib.pyplot as pyplot
+
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
+
 
 data = pandas.read_csv('clustering.csv')
 print(data.head())
@@ -13,19 +16,38 @@ print(data.columns)
 subset = data[["LoanAmount","ApplicantIncome"]]
 subset = subset.values
 
-# DATA VISUALIZATION:
-pyplot.scatter(subset["ApplicantIncome"],subset["LoanAmount"])
+# # DATA VISUALIZATION:
+pyplot.scatter(subset[:,0],subset[:,1])
 pyplot.xlabel('AnnualIncome')
 pyplot.ylabel('Loan Amount (In Thousands)')
 pyplot.savefig("scatter_loanamount_applicantincome.png")
 pyplot.close()
 
+def run_kmeans(n, data):
+	machine = KMeans(n_clusters = n) # Constructing the machine
+	machine.fit(subset) # fitting the data
+	results = machine.predict(subset) # predicting
 
-machine = KMeans(n_clusters = 2) # Constructing the machine
-machine.fit(subset) # fitting the data
-results = machine.predict(subset) # predicting
+	centroids = machine.cluster_centers_
+	ssd = machine.inertia_ #sum of square deviations
+	
+	silhouette = 0
+	if n>1:
+		silhouette = silhouette_score(data, machine.labels_, metric = 'euclidean')
 
-centroids = machine.cluster_centers_
+	pyplot.scatter(data[:,0], data[:,1], c = results) # row, column (all rows, first column)
+	pyplot.scatter(centroids[:,0], centroids[:,1], c='red', marker= "*", s=200)
+	pyplot.savefig("scatterplot_colors_"+ str(n) + ".png") # no need for this if using Jupyter Notebook
+	pyplot.close()
+
+	return ssd, silhouette
+
+
+result = [run_kmeans(i + 1, data) for i in range (10)]
+print(result)
+
+result_diff = [result[i-1] - x for i,x in enumerate(results)]
+print(result_diff)
 
 # Visualization of results: 
 # Scatterplot (x, y)
@@ -34,3 +56,14 @@ pyplot.scatter(centroids[:,0], centroids[:,1], c='red', marker= "*", s=200)
 pyplot.savefig("scatterplot_la_colors.png")
 pyplot.close()
 
+pyplot.plot(range(2,8), silhouette_result)
+pyplot.savefig("silhouette.png")
+pyplot.close()
+
+print("\nssd: \n", ssd_result)
+print("\nssd differences: \n", ssd_result_diff)
+
+
+print("\nsilhouette scores: \n", silhouette_result)
+print("\nmax silhouette scores: \n", max(silhouette_result))
+print("\nnumber of cluster with max silhouette scores: \n", silhouette_result.index(max(silhouette_result))+2)
