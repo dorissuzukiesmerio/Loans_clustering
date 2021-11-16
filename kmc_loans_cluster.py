ce8261ab0d761aebad28cfe23d73296120ee0d5f
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-11-16 12:59:39 -0500

    adjustments

--- kmc_loans_cluster.py
+++ kmc_loans_cluster.py
@@ -23,9 +23,9 @@ pyplot.ylabel('Loan Amount (In Thousands)')
 pyplot.savefig("scatter_loanamount_applicantincome.png")
 pyplot.close()
 
-def run_kmeans(n, data):
+def run_kmeans(n, subset):
 	machine = KMeans(n_clusters = n) # Constructing the machine
-	machine.fit(subset) # fitting the data
+	machine.fit(subset) # fitting the subset
 	results = machine.predict(subset) # predicting
 
 	centroids = machine.cluster_centers_
@@ -33,28 +33,37 @@ def run_kmeans(n, data):
 	
 	silhouette = 0
 	if n>1:
-		silhouette = silhouette_score(data, machine.labels_, metric = 'euclidean')
+		silhouette = silhouette_score(subset, machine.labels_, metric = 'euclidean')
 
-	pyplot.scatter(data[:,0], data[:,1], c = results) # row, column (all rows, first column)
+	pyplot.scatter(subset[:,0], subset[:,1], c = results) # row, column (all rows, first column)
 	pyplot.scatter(centroids[:,0], centroids[:,1], c='red', marker= "*", s=200)
 	pyplot.savefig("scatterplot_colors_"+ str(n) + ".png") # no need for this if using Jupyter Notebook
 	pyplot.close()
 
 	return ssd, silhouette
 
+# result =[]
+# for i in range(10):
+# 	ssd = run_kmeans(i+1, subset)
+# 	result.append(ssd)
 
-result = [run_kmeans(i + 1, data) for i in range (10)]
+result = [run_kmeans(i + 1, subset) for i in range (10)]
 print(result)
 
-result_diff = [result[i-1] - x for i,x in enumerate(results)]
+# result_diff=[]
+# for counter, value enumerate(result):
+# 	ssd_diff = result[counter-1] - value
+# #call counter/index = i , value/element = x
+
+result_diff = [result[i-1] - x for i,x in enumerate(result)][1:]
 print(result_diff)
 
 # Visualization of results: 
 # Scatterplot (x, y)
-pyplot.scatter(subset[:,0], subset[:,1], c = results) # row, column (all rows, first column)
-pyplot.scatter(centroids[:,0], centroids[:,1], c='red', marker= "*", s=200)
-pyplot.savefig("scatterplot_la_colors.png")
-pyplot.close()
+# pyplot.scatter(subset[:,0], subset[:,1], c = results) # row, column (all rows, first column)
+# pyplot.scatter(centroids[:,0], centroids[:,1], c='red', marker= "*", s=200)
+# pyplot.savefig("scatterplot_la_colors.png")
+# pyplot.close()
 
 pyplot.plot(range(2,8), silhouette_result)
 pyplot.savefig("silhouette.png")
